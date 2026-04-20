b5f850ebfe3320a38288887166b6f9cd62d4e57e
Your Name <user@example.com>
2026-04-20 12:38:41 +0200

    small edits

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 title War Era - Jet Market Analyzer
 echo 🔍 جاري تشغيل محلل سوق الطائرات...
 echo ====================================
-cd /d C:\Users\3BBADI\desktop\war_era_analyzer
+cd /d C:\Users\3BBADI\Desktop\New folder\war_era_analyzer-Manus-Test
 python -m streamlit run app.py --server.headless true
 pause
